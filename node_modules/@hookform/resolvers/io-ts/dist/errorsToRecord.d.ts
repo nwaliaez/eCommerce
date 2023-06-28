1a07c46fea7726066b3b27c44fbe435cb561d68b
Neeraj Walia <122222243+nwaliaez@users.noreply.github.com>
2023-06-29 01:53:55 +0530

    Delete node_modules directory
    unwanted

--- node_modules/@hookform/resolvers/io-ts/dist/errorsToRecord.d.ts
+++
@@ -1,5 +0,0 @@
-import { ValidationError } from 'io-ts';
-import * as ReadonlyArray from 'fp-ts/ReadonlyArray';
-import { ErrorObject } from './types';
-declare const errorsToRecord: (validateAllFieldCriteria: boolean) => (validationErrors: ReadonlyArray<ValidationError>) => ErrorObject;
-export default errorsToRecord;
